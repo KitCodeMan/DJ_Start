3f79dc43694b49a03f7f7b5b491157ad241192d7
QSCZAQWSXEDC <QSCZAQWSXEDC@gmail.com>
2024-12-27 12:22:44 +0330

    first step to wite is newyear app

--- Web_PROJECT/Newyear/views.py
+++ Web_PROJECT/Newyear/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+import datetime
 
+
+def index(request):
+    now=datetime.datetime.now()
+    return render(request,"Newyear/index.html",{
+        "newyear":now.month==1 and now.day==1
+        })
 # Create your views here.
